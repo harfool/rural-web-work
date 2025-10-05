ff51fc28cafbf6b5dd8f9e6e2f149c3efd46b578
harfool <harfoolgujjar63@gmail.com>
2025-10-05 13:07:30 +0530

    fixed typo

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,7 @@ export default function Home() {
   return (
     <div className="h-screen flex justify-center items-center text-7xl font-extrabold">
       <h1>
-        <span className="text-[#FF671F]">Rurla</span>
+        <span className="text-[#FF671F]">Rural</span>
         <span className="text-[#FFFFFF]">Web</span>
         <span className="text-[#046A38]">Work</span>
       </h1>
