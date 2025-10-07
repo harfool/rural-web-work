734fccb2e5ce956670c2f05ba5573e8ee92eb259
sandeepatel01 <sandeepatel.0101@gmail.com>
2025-10-07 12:19:24 +0530

    feat: Add navbar

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,11 @@
+"use client";
+
 export default function Home() {
   return (
-    <div className="h-screen flex justify-center items-center text-7xl font-extrabold bg-zinc-800">
+    <div className="h-screen flex justify-center items-center text-7xl font-extrabold bg-white">
       <h1>
         <span className="text-[#FF671F]">Growify</span>
-        <span className="text-[#FFFFFF]">India</span>
+        <span className="text-gray-400">India</span>
         <span className="text-[#046A38]">Work</span>
       </h1>
     </div>
